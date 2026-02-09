8e1eba592a8b89bb5551764727ce1482153015d1
Meshack Yaro <yaromeshack@gmail.com>
2026-02-09 16:51:25 +0100

    refactor authentication and jwt

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
-import jwt from "jsonwebtoken";
-import { env } from "../config/env.js";
+import { verifyToken } from "../utils/jwt.js";
 
 export const protect = (req, res, next) => {
 
     const authHeader = req.headers.authorization;
 
-    if (!authHeader) res.status(401).json({message: "Unauthorized"});
+    if (!authHeader || !authHeader.startsWith("Bearer ")) 
+        res.status(401).json({message: "Unauthorized"});
 
     const token = authHeader.split(" ")[1];
 
     try {
 
-        const decoded = jwt.verify(token, env.JWT_SECRET);
+        const decoded = verifyToken;
         req.user = decoded;
         next();
 
