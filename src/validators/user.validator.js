5fb1d280ea9de47c09d2c5268ed45aae9cf95cba
Meshack Yaro <yaromeshack@gmail.com>
2026-02-12 23:58:37 +0100

    feat: add change password implementation

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -9,3 +9,7 @@ export const updateProfileSchema = z.object({
     email: z.email("Invalid email address"),
     // password: z.string().min(6, "Password must be at least 6 characters long")
 });
+
+export const changePasswordSchema = z.object({
+    
+});
